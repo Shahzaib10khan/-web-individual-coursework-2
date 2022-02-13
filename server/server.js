c9d49222c00ef6842ab1c88b19b779c93330a500
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2022-02-13 20:27:34 +0500

    Mongoodb connected

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('mongodb');
 
 const app = express();
 
@@ -9,6 +11,13 @@ app.use(cors())
 app.get('/hello', (req, res) => res.send('hello'))
 
 const Port = process.env.PORT || 5000;
+const url = 'mongodb+srv://vue_rest_api:12345@cluster0.bifqg.mongodb.net/vueRestAPI?retryWrites=true&w=majority';
+
+MongoClient.connect(url, function(err, client) {
+    console.log('db connected!');
+    db = client.db('vueRestAPI');
+})
+
 
 
 app.listen(Port, () => console.log('Server is running...'))
